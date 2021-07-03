da32f6169f370ff530961d47e8d83146d6e45f61
Syline Khamphousone <syline.khamphousone@viveris.fr>
2021-07-03 19:00:55 +0200

    fix(authentication): if token expired, ask to login again

--- front/src/app/core/interceptors/server-error.interceptor.ts
+++ front/src/app/core/interceptors/server-error.interceptor.ts
@@ -1,5 +1,6 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { NotificationsService } from '../services/notifications.service';
@@ -9,15 +10,30 @@ export class ServerErrorInterceptor implements HttpInterceptor {
 
   constructor(
     private notificationsService: NotificationsService,
+    private router: Router,
   ) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((err) => {
+        if (this.isUnauthorized(err)) {
+          this.redirectToHomepage();
+          return of(err);
+        }
+
         this.notificationsService.showErrorMessage(this.getErrorMessage(err));
         return of(err);
       }),
     );
+  }
+
+  private isUnauthorized(err): boolean {
+    return err.error && err.error.statusCode === 401;
+  }
+
+  private redirectToHomepage(): void {
+    this.notificationsService.showInformation('Veuillez vous reconnecter');
+    this.router.navigate(['/homepage']);
   }
 
   private getErrorMessage(err): string {
