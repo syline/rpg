fa0006e69900935e47e88f42e34e5ab8c83cb751
Syline Khamphousone <syline.khamphousone@viveris.fr>
2021-07-05 23:43:00 +0200

    ci: configure test coverage

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -9,18 +9,23 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/front'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'src/**/*.js': ['coverage']
     },
-    reporters: ['progress', 'kjhtml'],
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
