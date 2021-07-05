22dddd46f201fa7f8eb9f6ee3f472a537b78adf4
Syline Khamphousone <syline.khamphousone@viveris.fr>
2021-07-05 23:20:39 +0200

    refactor: remove useless comment

--- front/src/app/shared/models/character.ts
+++ front/src/app/shared/models/character.ts
@@ -42,22 +42,6 @@ export class Character {
   }
 
   getCostToLevelUp(characteristic: CharacteristicEnum): number {
-    // let cost: number;
-    // switch (characteristic) {
-    //   case CharacteristicEnum.attack:
-    //     cost = Math.ceil(this.attack / 5);
-    //     break;
-    //   case CharacteristicEnum.defense:
-    //     cost = Math.ceil(this.defense / 5);
-    //     break;
-    //   case CharacteristicEnum.health:
-    //     cost = 1;
-    //     break;
-    //   case CharacteristicEnum.magik:
-    //     cost = Math.ceil(this.magik / 5);
-    //     break;
-    // }
-    // return cost;
     if (characteristic === CharacteristicEnum.health) {
       return 1;
     }
@@ -77,20 +61,6 @@ export class Character {
 
     this.skills -= this.getCostToLevelUp(characteristic);
     this[characteristic]++;
-    // switch (characteristic) {
-    //   case CharacteristicEnum.attack:
-    //     this.attack++;
-    //     break;
-    //   case CharacteristicEnum.defense:
-    //     this.defense++;
-    //     break;
-    //   case CharacteristicEnum.health:
-    //     this.health++;
-    //     break;
-    //   case CharacteristicEnum.magik:
-    //     this.magik++;
-    //     break;
-    // }
   }
 
   levelDown(characteristic: CharacteristicEnum): void {
